7586b5f580d585ffb2941bc0043771312e0fa639
Matt Belcher <matt@mattbelcher.com>
2014-01-30 23:44:53 -0500

    new modern name format, randomly select between old and new names

--- vgng.js
+++ vgng.js
@@ -9,93 +9,74 @@ $("div#new_name").live( "click", function(event) {
     $('a#tweet').each(function(){
 	    $(this).attr('href', ('http://twitter.com/share?url=http://videogamena.me/&text=I made "' + $("div#video_game_name0").text() + '" with the Video Game Name Generator! Make your own: '));
 	});
-    });
+});
+
+var prepositions = []
+function build_prep_list(prep_list) {
+    prepositions = prep_list.split("\n");
+}
 
 var word_list = new Array(3);
-
 function build_list(big_list) {
-  var words = big_list.split("\n");
-  window.word_list[0] = []
-  var word_list_index = 0;
-  for (var word in words) {
-    if (words[word] == "----") {
-      word_list_index++;
-      window.word_list[word_list_index] = []
-    } else {
-      window.word_list[word_list_index].push(words[word]);
+    var words = big_list.split("\n");
+    window.word_list[0] = []
+    var word_list_index = 0;
+    for (var word in words) {
+        if (words[word] == "----") {
+            word_list_index++;
+            window.word_list[word_list_index] = []
+        } else {
+            window.word_list[word_list_index].push(words[word]);
+        }
     }
-  }
+}
+
+function generate_word(i, bad_match_list) {
+    var allow_similar_matches = !$('#similar_terms').is(':checked');
+
+    var word = window.word_list[i][Math.floor(Math.random()*window.word_list[i].length)]
+    var bad_words = ""
+    if (word.indexOf("^") != -1) {
+        bad_words = word.split("^")[1]
+        word = word.split("^")[0]
+    }
+
+    if ($.inArray(word, bad_match_list) != -1) {
+        return generate_word(i, bad_match_list)
+    }
+    bad_match_list.concat(word)
+    bad_match_list.concat(bad_words)
+
+    return word
+}
+
+function generate_modern_game_name() {
+    var bml = new Array()
+    return generate_word(0, bml) + " " + generate_word(1, bml) + ": " +
+        generate_word(0, bml) + " " + generate_word(2, bml)
+}
+
+function generate_old_game_name() {
+    var bml = new Array()
+    return generate_word(0, bml) + " " + generate_word(1, bml) + " " + generate_word(2, bml)
 }
 
 function generate_game_name() {
-  var first_word = window.word_list[0][Math.floor(Math.random()*window.word_list[0].length)];
-  var second_word = "";
-  var third_word = "";
-  var bad_match_list = new Array();
-
-  var allow_similar_matches = !$('#similar_terms').is(':checked');
-
-  if (first_word.indexOf("^") != -1) {
-      if (!allow_similar_matches) {
-	  bad_match_list = first_word.split("^")[1].split('|');    
-      }
-      first_word = first_word.split("^")[0]; 
-  } 
-
-  var second_word_bad = true;
-  while (second_word_bad) {
-    second_word = window.word_list[1][Math.floor(Math.random()*window.word_list[1].length)];
-    if (second_word.indexOf("^") != -1) {
-	if (!allow_similar_matches) {
-	    bad_match_list.concat(second_word.split('^')[1].split('|'));
-	}
-	second_word = second_word.split('^')[0];                           
-    }
-
-    if (second_word == first_word) {
-	continue;
-    }
-
-    if ($.inArray(second_word, bad_match_list) != -1) {
-	continue;
-    }
-    second_word_bad = false;
-  }
-
-  var third_word_bad = true;
-  while (third_word_bad) {
-    third_word = window.word_list[2][Math.floor(Math.random()*window.word_list[2].length)];
-
-    if (third_word.indexOf("^") != -1) {
-	if (!allow_similar_matches) {
-	    bad_match_list.concat(third_word.split('^')[1].split('|'));
-	}
-	third_word = second_word.split('^')[0];                           
-    }
-
-    if (third_word == first_word || third_word == second_word) {
-	continue;
-    }
-
-    if ($.inArray(third_word, bad_match_list) != -1) {
-	continue;
-    }
-    third_word_bad = false;
-  }  
-  return first_word + " " + second_word + " " + third_word;
+    var PROB_OLD = 0.50
+    return [generate_old_game_name(), generate_modern_game_name()][Math.random() < PROB_OLD ? 0 : 1]
 }
 
 function addLoadEvent(func) {
-  var oldonload = window.onload;
-  if(typeof window.onload != 'function') {
-    window.onload = func;
-  }
-  else {
-    window.onload = function() {
-      if(oldonload) {
-        oldonload();
-      }
-    func();
+    var oldonload = window.onload;
+    if(typeof window.onload != 'function') {
+        window.onload = func;
     }
-  }
+    else {
+    window.onload = function() {
+        if(oldonload) {
+            oldonload();
+        }
+        func();
+    }
+    }
 }
